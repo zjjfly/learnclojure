f269979330ca49935a02c96e9f85b76ede0a74d6
zjjfly <zjjblue@gmail.com>
2015-09-30 16:41:43 +0800

    加入if和loop相关的内容

--- src/learnclojure/chapt1/core6.clj
+++ src/learnclojure/chapt1/core6.clj
@@ -25,4 +25,39 @@
 
 ;;条件判断：if
 ;;if是clojure唯一的基本条件判断
+;; if后第一个条件表达式是逻辑True（任何非nil和false的值）的话，那么整个表达式的值是第二个表达式的值，否则是第三个表达式的值
+(if "hi" \t)
+(if 43 \t)
+(if nil "adad" \f)
+(if false "adad" \f)
+;;如果条件表达式是false，但else表达式没有提供，则整个if的值是nil，这种情况最好使用when
+(if (not true) \t)
+;;true?和false？这两个谓词和if条件判断无关,它们检查的是参数是否是布尔值true和false，而不是逻辑上的true和false
+(true? "ad")
+(if "ad" \t \f)
+;;逻辑上的true和下面的式子等价
+#(or (not  (nil? %)) (true? %))
+
+;;循环：loop和recur
+;;clojure有好几个有用的循环结构，比如doseq和dotimes，都是构建在recur之上的
+;;recur能在不消耗堆栈空间的情况下吧程序执行转到上下文最近的loop头那里
+;;loop形式最后一个表达式如果产生一个值，则这个值就作为loop形式的值
+(loop [x 5]
+  (println x)
+  (if (neg? x)
+    x
+    (recur (- x 4))))
+;;函数也可以建立loop头
+(defn countdown
+  "hehe"
+  [x]
+  (if (zero? x)
+    :blastoff!
+    (do (println x)
+        (recur (dec x)))))
+(countdown 5)
+;;recur 是非常底层的循环和递归控制操作，通常不用，但它不消耗堆栈空间，对于实现某些递归算法很关键
+;;并且它不对数字装箱，所以对于实现一些对于数学和数据有关的操作很有用
+;;还有一些情况需要累计或者消费一个或多个集合，这也会用到recur
+
 
