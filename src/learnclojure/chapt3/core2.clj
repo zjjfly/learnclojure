5eafe15694a12595f38a7578a3959ffbacc57b19
zjjfly <zjjblue@gmail.com>
2015-10-20 21:57:55 +0800

    sequece接口

--- src/learnclojure/chapt3/core2.clj
+++ src/learnclojure/chapt3/core2.clj
@@ -43,3 +43,54 @@
 (count [1 2 4 5])
 ;;count保证所有对于所有集合操作耗时都是高效的，序列除外（因为它的长度可能是未知的）
 
+;;Sequence接口定义了一个获取和遍历各种集合的一个顺序视图的方法
+;;常用函数：
+;;seq返回传入参数的一个序列
+;;first，rest，next提供了遍历序列的方法
+;;lazy-seq创建一个内容是一个表达式结果的惰性序列
+
+;;seq支持的参数类型：
+;;1.所以clojure集合类型
+;;2.所有java集合类型,即java.util.*
+;;3.所有java map
+;;4.所有java.lang.CharSequence，包括String
+;;5.实现了java.lang.Iterable的类型
+;;6.数组
+;;7.nil（或者java方法返回的null）
+;;8.任何实现了clojure.lang.Seqable接口的类型
+;;一部分例子：
+(seq "Clojure")
+;=(\C \l \o \j \u \r \e)
+(seq {:a 5 :b 6})
+;=([:a 5] [:b 6])
+(seq (java.util.ArrayList. (range 5)))
+(seq (into-array ["Clojure" "Programming"]))
+;=("Clojure" "Programming")
+(seq [])
+;=nil
+(seq nil)
+;=nil
+;;seq对于nil和空集合都返回nil
+
+;;许多和序列打交道的函数都隐式调用了seq,比如:
+(map str "Clojure")
+(set "Programming")
+
+;;如果用现有的序列函数来定义自己的操作序列的函数，不需要对参数调用seq，除了使用lazy-seq
+
+;;clojure标准库clojure.core也有很多方法创建操作序列，最基本的是first，rest，next
+(first "Clojure")
+(rest "Clojure")
+(next "Clojure")
+;;rest和next大多数情况是一样的，对待空集合和只有一个元素的集合它们不一样
+(rest "")
+;=()
+(next "")
+;=nil
+(rest "1")
+;=()
+(next "1")
+;=nil
+(= (next "1")
+   (seq (rest "1")))
+;;这个区别看起来很小 ，但使我们很容易实现惰性序列
