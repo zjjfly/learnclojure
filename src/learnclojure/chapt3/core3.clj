23604188a180b12126c7df97660a6a7b1679987c
zjjfly <zjjblue@126.com>
2015-10-26 23:04:37 +0800

    惰性序列结束

--- src/learnclojure/chapt3/core3.clj
+++ src/learnclojure/chapt3/core3.clj
@@ -9,16 +9,51 @@
     (println "realizing random int")
     (cons (rand-int limit)
           (random-ints limit))))
-//定义一个惰性序列，但不会实例化
+;;定义一个惰性序列，但不会实例化
 (def rands (take 10 (random-ints 10)))
-//求第一个元素，使我们定义lazy-seq时传入的表达式被调用一次，得到一个随机数，并打印一次信息
+;;求第一个元素，使我们定义lazy-seq时传入的表达式被调用一次，得到一个随机数，并打印一次信息
 (first rands)
-//求中间一个元素，在这个元素之前的还没有实例化的元素都会被实例化
+;;求中间一个元素，在这个元素之前的还没有实例化的元素都会被实例化
 (nth rands 3)
-//对所有的元素实例化（已经实例化过的会被保存，不用再实例化）
+;;对所有的元素实例化（已经实例化过的会被保存，不用再实例化）
 (count rands)
 ;;cons，list*不会强制对传入的序列（可能是惰性的）求值
 ;;这一点使得我们可以传入一个或多个值以及一个惰性序列来创建一个更大的惰性序列
 ;;random-ints实现的很差，一般不这么写，更好的写法是：
 (repeatedly 10 (partial rand-int 10))
+;;只包含一个参数的repeatedly会返回一个有无限元素的惰性序列，clojure中处理无限长度的序列很平常
+;;clojure标准库和社区的一些库有很多透明处理惰性序列的函数，而且所有这些核心的序列处理函数都返回惰性序列
+;;比如：map、for、filter、take、drop以及由它们引申出来的一些函数（take-nth、remove、drop-while等）
+;;有了这些工具我们可以把许多问题归结为对一个序列的值的处理
+
+;;如果序列中每个元素的实例化需要一些io操作或者计算量很大，那么要非常小心的处理。这也是rest和next很大的一个区别所在。
+(def x (next (random-ints 10)))
+;;next实例化两次
+(def x (rest (random-ints 10)))
+;;rest实例化一次
+
+;;顺序解构始终使用next而不是rest
+(let [[_ & _] (random-ints 10)])
+;;实例化两次
+
+;;要完全实例化一个惰性序列，如果需要保持这个序列的所以元素，用doall，只想要实例化的时候的副作用，用dorun
+(doall (take 5 (random-ints 10)))
+(dorun (take 5 (random-ints 10)))
+
+;;只要保持了对序列的一个引用，那么序列中的元素不能被垃圾回收，这就是“头保持”
+;;如果序列很大，容易发生内存溢出问题
+;;split-with是这样一个函数：给它一个谓词函数，一个可序列化的值，返回两个惰性序列，第一个满足这个谓词函数的，第二个是不满足的,例子：
+(split-with neg? [-2 -1 0 1 2])
+;;下面这段代码就有头保持问题
+;(let [[t d] (split-with #(< % 4) (range 1e8))]
+  ;[(count d) (count t)])
+;;把部分代码交换一下就不会出问题
+(let [[t d] (split-with #(< % 4) (range 1e8))]
+[(count t) (count d)])
+;;原因是由于编译器发现序列的前四个元素还会被使用，所有没有回收这些元素，导致整个惰性序列都实例化了但没有被回收
+;;所有会占用非常多内存
+;;参考资料：http://xumingming.sinaapp.com/977/clojure-lazyseq-head-retention/
+;;https://groups.google.com/forum/?fromgroups=#!topic/clojure/bTAYeLXc25w
+
+;;向map、set中插入元素，=函数或者count函数都是头保持问题的诱因，因为它们都会强制对惰性序列实例化
 
