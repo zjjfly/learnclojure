14d2d516e84778af3b5e9efa9dbfa75e80f295a7
zjjfly <zjjblue@126.com>
2015-10-28 01:57:22 +0800

    Associative抽象

--- src/learnclojure/chapt3/core4.clj
+++ src/learnclojure/chapt3/core4.clj
@@ -1,4 +1,5 @@
-(ns learnclojure.chapt3.core4)
+(ns learnclojure.chapt3.core4
+  (:import (java.util HashMap)))
 ;;Associative抽象
 ;;关系型数据结构Associative接口抽象的是把一个key和一个value关联起来的数据结构
 ;;这个接口有四个操作：
@@ -32,5 +33,40 @@
   1 4
   0 -12
   2 :p)
+;;可以用assoc实现和conj一样的效果，把一个值添加到vector最后，但需要显式的告诉vector新的下标
+(assoc v 3 "ddd")
+;;get还能操作set，如果一个key在set中存在，会返回它
+(def s #{1 2 4})
+(get s 1)
+(get s 3)
+(get s 3 "not found!")
+;;它的返回值意味着set里面是值到值本身的映射，这使得set可以满足get的语义，同时也和条件判断里的经典用法一致
+(if (get s 3)
+  (print "it contains '3'")
+  (print "it doesn't contain '3'"))
+;;clojure中map和set本身就支持get语义
+(s 1)
+(m :b)
+
+;;contains?是一个谓词，作用是检查集合中是否包含指定的key
+(contains? m :d)
+(contains? v 3)
+(contains? s 3)
+;;初学者会用contains？检测vector中是否含有指定的值，这是不对的.contains？检测是集合中是否包含某个key
+;;检测集合中是否包含某个值用some
+
+;;get和contains？非常同意，可以高效的操作vector,map,set,java里的map，字符串以及java数组
+(get "clojure" 3)
+(contains? (HashMap.) 1)
+(get (into-array [1 2 3]) 0)
+
+;;小心nil
+;;如果集合中不包含某个key，调用的时候也没有设置默认值,那么会返回nil、但这个key对于的值也可能是nil
+(get {:ethl nil} :ethl)
+;;那么怎么知道是没有这个key还是这个key的值是nil呢？
+;;我们可以使用find，它返回的是键值对如果map里包含指定的key的话，返回nil如果不包含这个key
+(find {:ethl nil} :ethl)
+(find {:ethl nil} :r)
+
 
 
