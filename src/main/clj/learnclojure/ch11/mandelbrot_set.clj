71d50505f6eb4cfe2c0357f6e92246d82d791bc2
zjjfly <zjjblue@gmail.com>
2019-11-30 12:09:40 +0800

    complete chapter 11

---
+++ src/main/clj/learnclojure/ch11/mandelbrot_set.clj
@@ -0,0 +1,84 @@
+(ns learnclojure.ch11.mandelbrot-set
+  (:import java.awt.image.BufferedImage
+           (java.awt Color RenderingHints Graphics2D)
+           (javax.imageio ImageIO)))
+
+(defn- escape
+  [a0 b0 depth]
+  (loop [a a0
+         b b0
+         iteration 0]
+    (cond
+      (< 4 (+ (* a a) (* b b))) iteration
+      (>= iteration depth) -1
+      :else (recur (+ a0 (- (* a a) (* b b)))
+                   (+ b0 (* 2 (* a b)))
+                   (inc iteration)))))
+
+(defn mandelbrot
+  [rmin rmax imin imax & {:keys [width height depth]
+                          :or   [width 80 height 49 depth 1000]}]
+  (let [rmin (double rmin)
+        imin (double imin)
+        stride-w (/ (- rmax rmin) width)
+        stride-h (/ (- imax imin) height)]
+    (loop [x 0
+           y (dec height)
+           escapes []]
+      (if (= x width)
+        (if (zero? y)
+          (partition width escapes)
+          (recur 0 (dec y) escapes))
+        (recur (inc x) y (conj escapes (escape (+ rmin (* x stride-w))
+                                               (+ imin (* y stride-h))
+                                               depth)))))))
+(defn render-text
+  [mandelbrot-grid]
+  (doseq [row mandelbrot-grid]
+    (doseq [escape-iter row]
+      (print (if (neg? escape-iter) \* \space))
+      println)))
+
+(defn render-image
+  [mandelbrot-grid]
+  (let [palette (vec (for [c (range 500)]
+                       (Color/getHSBColor 0.0 0.0 (/ (Math/log c) (Math/log 500)))))
+        height (count mandelbrot-grid)
+        width (count (first mandelbrot-grid))
+        img (BufferedImage. width height BufferedImage/TYPE_INT_RGB)
+        ^Graphics2D g (.getGraphics img)]
+    (doseq [[y row] (map-indexed vector mandelbrot-grid)
+            [x escape-iter] (map-indexed vector row)]
+      (.setColor g (if (neg? escape-iter)
+                     (palette 0)
+                     (palette (mod (dec (count palette)) (inc escape-iter)))))
+      (.drawRect g x y 1 1))
+    (.dispose g)
+    img))
+
+(render-text (mandelbrot -2.25 0.75 -1.5 1.5 :width 80 :height 40 :depth 100))
+
+;但如果要放大这个图,需要对escape进行优化
+(do (time (mandelbrot -2.25 0.75 -1.5 1.5
+                       :width 1600 :height 1200 :depth 1000))
+    nil)
+;Elapsed time: 1465.814648 msecs
+(defn- escape
+  [^double a0 ^double b0 depth]
+  (loop [a a0
+         b b0
+         iteration 0]
+    (cond
+      (< 4 (+ (* a a) (* b b))) iteration
+      (>= iteration depth) -1
+      :else (recur (+ a0 (- (* a a) (* b b)))
+                   (+ b0 (* 2 (* a b)))
+                   (inc iteration)))))
+
+(defn draw
+  [img name]
+  (ImageIO/write img "png" (clojure.java.io/file (str name ".png"))))
+
+(draw (render-image (mandelbrot -2.25 0.75 -1.5 1.5 :width 800 :height 800 :depth 500)) "mandelbrot1")
+
+(draw (render-image (mandelbrot -1.5 -1.3 -0.1 0.1 :width 800 :height 800 :depth 500)) "mandelbrot2")
--- src/main/clj/learnclojure/ch11/optimize.clj
+++ src/main/clj/learnclojure/ch11/optimize.clj
@@ -125,13 +125,51 @@
 (defn array-histogram
   [data]
   (vec
-   (reduce (fn [^longs hist v]
-             (aset hist v (inc (aget hist v)))
-             hist)
-           (long-array 10)
-           data)))
+    (reduce (fn [^longs hist v]
+              (aset hist v (inc (aget hist v)))
+              hist)
+            (long-array 10)
+            data)))
 (time (array-histogram data))
 ;Elapsed time: 23.919621 msecs
+
+;如果想要把已有的集合转换成数组,可以使用into-array和to-array
+
+;into-array返回的是集合的第一个元素的类型的数组,或是指定的超类型的数组
+(into-array ["a" "b" "c"])
+;#object["[Ljava.lang.String;" 0x6807f1a0 "[Ljava.lang.String;@6807f1a0"]
+
+(into-array CharSequence ["a" "b" "c"])
+;#object["[Ljava.lang.CharSequence;" 0x361b70f3 "[Ljava.lang.CharSequence;@361b70f3"]
+;上面这种指定数组元素类型的方式在与Java进行互操作的时候比较有用
+
+;这种方式也可以用于生成原始类型的数组
+(into-array Long/TYPE [1 2 3])
+;#object["[J" 0x13a24756 "[J@13a24756"]
+
+;clojure提供了一些辅助函数用于创建原始类型的数组或引用数组:boolean-array,byte-array,short-array,char-array,int-array
+;long-array,float-array,double-array,object-array
+;它们的参数可以使一个指定数组大小的数字或一个集合
+(long-array 10)
+;#object["[J" 0x5880530d "[J@5880530d"]
+(long-array (range 10))
+;#object["[J" 0x2a6757a8 "[J@2a6757a8"]
+;或者可以同时传入这两个参数,但如果集合的大小比数组的容量小,那么会使用这个数组的元素类型的默认值填充
+(seq (long-array 20 (range 10)))
+;(0 1 2 3 4 5 6 7 8 9 0 0 0 0 0 0 0 0 0 0)
+
+;make-array用于创建任意大小或维度的数组,用所给类型的默认值初始化
+(def arr (make-array String 5 5))
+(aget arr 0 0)
+;nil
+(def arr (make-array Boolean/TYPE 10))
+(aget arr 0)
+;false
+;当需要获取数组的类型,尤其是多维数组的类型的时候,需要使用class函数,但这样做必须有一个数组的实例,这样会让人感觉很奇怪
+;还有一种做法是clj_operate_java.clj中的array-type函数
+;clojure在不知道数组的类型的时候,访问和修改其中的元素会使用反射,这与把数组作为局部优化是相悖的
+;所以clojure提供了一组专门用于数组的类型提示:
+;^objects,^booleans,^bytes,^chars,^longs,^ints,^shorts,^doubles,^floats
 
 ;aget和aset也要求数组类型是已知的来避免使用反射
 (let [arr (long-array 10)]
@@ -145,12 +183,63 @@
 (let [a (int-array (range 10))]
   (vec
     (amap a i res
-         (inc (aget a i)))))
+          (inc (aget a i)))))
 ;[1 2 3 4 5 6 7 8 9 10]
 ;amap的第一个参数时源数组,第二个参数是下标的名称,第三个是结果数组的名称,第四个是一个表达式,它的结果会设置为结果数组的下标i的值
 
 (let [a (int-array (range 10))]
   (areduce a i sum 0
-        (+ sum (aget a i))))
+           (+ sum (aget a i))))
 ;前面两个参数和amap是一样的,第三个参数是累加器的名称,第四个是累加器的初始值,最后是一个表达式,它的值会成为下一次迭代的累加器的值和迭代结束之后的返回结果
 
+;当操作多维数组是,要特别小心
+(def arr (make-array Double/TYPE 1000 1000))
+(time (dotimes [i 1000]
+        (dotimes [j 1000]
+          (aset arr i j 1.0)
+          (aget arr i j))))
+;Elapsed time: 11969.139812 msecs
+;可以看出上面的代码效率很低,原因是在操作多维数组的时候,aset被apply传播到其他参数时,1.0会被封装(因为apply的第一个参数是IFn类型)
+;提高性能的方式是手动展开多维数组
+(time (dotimes [i 1000]
+        (dotimes [j 1000]
+          (let [^doubles darr (aget ^objects arr i)]
+            (aset darr j 1.0)
+            (aget darr j)))))
+;Elapsed time: 16.564624 msecs
+
+;作者提供了两个宏来实现对多维数字自动解包和加类型提示
+(defmacro deep-aget
+  ([array idx]
+   `(aget ~array ~idx))
+  ([array idx & idxs]
+   (let [a-sym (gensym "a")]
+     `(let [~a-sym (aget ~(vary-meta array assoc :tag 'objects) ~idx)]
+        (deep-aget ~(with-meta a-sym {:tag (-> array meta :tag)}) ~@idxs)))))
+
+(defmacro deep-aset
+  [array & idxsv]
+  (let [hints '{booleans boolean,bytes byte
+                chars char, longs long
+                ints int ,shorts short
+                doubles double,floats float}
+        hint (-> array meta :tag)
+        [v idx & sxdi ] (reverse idxsv)
+        idxs (reverse sxdi)
+        v (if-let [h (hints hint)] (list h v) v)
+        nested-array (if (seq idxs)
+                       `(deep-aget ~(vary-meta array assoc :tag 'objects) ~@idxs)
+                       array)
+        a-sym (gensym "a")]
+    `(let [~a-sym ~nested-array]
+       (aset ~(with-meta a-sym {:tag hint}) ~idx ~v))))
+;使用deep-aget和deep-aset实现之前的代码
+(time (dotimes [i 1000]
+        (dotimes [j 1000]
+          (deep-aset ^doubles arr i j 1.0)
+          (deep-aget ^doubles arr i j))))
+
+;Elapsed time: 24.035916 msecs
+;to-array总是返回Object数组
+(to-array [1 2 3])
+(into-array [1 2 3])
