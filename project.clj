7c1e43cbcdf936cf2a901979abcc38e3acf2efaf
zijunjie <zjjblue@gmail.com>
2023-03-08 16:41:43 +0800

    update clojure

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  ; Database
                  [org.postgresql/postgresql "42.2.4"]
                  [com.mchange/c3p0 "0.9.5.2"]
